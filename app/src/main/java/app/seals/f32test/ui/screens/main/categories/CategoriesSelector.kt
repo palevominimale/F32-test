3e989bed6c787fdf1ed025cdfd68c66a5643b38e
palevominimale <badtri.orchestra@gmail.com>
2022-11-11 15:30:31 +0100

    small clean up

--- app/src/main/java/app/seals/f32test/ui/screens/main/categories/CategoriesSelector.kt
+++ app/src/main/java/app/seals/f32test/ui/screens/main/categories/CategoriesSelector.kt
@@ -2,19 +2,25 @@
 
 package app.seals.f32test.ui.screens.main.categories
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import app.seals.f32test.R
 import app.seals.f32test.ui.main.vm.MainActivityViewModel
 import app.seals.f32test.ui.models.categories.CategoryItemModel
 import app.seals.f32test.ui.states.UiState.IsLoading
 import app.seals.f32test.ui.states.UiState.IsReady
+import app.seals.f32test.ui.theme.Typography
 
 @Composable
 fun CategoriesSelector(vm: MainActivityViewModel) {
@@ -35,10 +41,19 @@ fun CategoriesRow(list: List<CategoryItemModel>, selected: String?, placeholders
     Surface(
         modifier = Modifier.padding(vertical = 16.dp)
     ) {
-        LazyRow {
-            itemsIndexed(list) { _, it ->
-                CategoryItem(item = it, placeholders = placeholders, selected = it.title == selected)
+        Column {
+
+            Text(
+                text = stringResource(id = R.string.main_select_category),
+                modifier = Modifier.padding(start = 16.dp, end = 16.dp, bottom = 8.dp),
+                style = Typography.bodyLarge
+            )
+            LazyRow {
+                itemsIndexed(list) { _, it ->
+                    CategoryItem(item = it, placeholders = placeholders, selected = it.title == selected)
+                }
             }
         }
+
     }
 }
--- app/src/main/java/app/seals/f32test/ui/screens/main/hotsales/HotSales.kt
+++ app/src/main/java/app/seals/f32test/ui/screens/main/hotsales/HotSales.kt
@@ -12,21 +12,24 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.blur
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import app.seals.f32test.R
 import app.seals.f32test.ui.main.vm.MainActivityViewModel
 import app.seals.f32test.ui.models.hotsales.HomeStoreItemModel
 import app.seals.f32test.ui.states.UiState
+import app.seals.f32test.ui.theme.Typography
 import coil.compose.rememberAsyncImagePainter
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
 import com.google.accompanist.pager.rememberPagerState
-import com.google.accompanist.placeholder.placeholder
 
 @Composable
 fun HotSales(vm: MainActivityViewModel) {
@@ -52,64 +55,102 @@ private fun Pager(list: List<HomeStoreItemModel>) {
     val isNew = remember { mutableStateOf(false) }
     val isBuy = remember { mutableStateOf(false) }
 
-    HorizontalPager(
-        count = list.size,
-        state = state,
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(200.dp)
-            .padding(vertical = 8.dp)
-    ) { page ->
-        imgUrl.value = list[page].picture.toString()
-        title.value = list[page].title.toString()
-        subtitle.value = list[page].subtitle.toString()
-        isNew.value = list[page].isNew ?: false
-        isBuy.value = list[page].isBuy ?: false
+    Column {
+        Text(
+            text = stringResource(id = R.string.main_hot_sales),
+            modifier = Modifier.padding(start = 16.dp, end = 16.dp, top = 8.dp),
+            style = Typography.bodyLarge
+        )
+        HorizontalPager(
+            count = list.size,
+            state = state,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(240.dp)
+                .padding(vertical = 16.dp)
+        ) { page ->
+            imgUrl.value = list[page].picture.toString()
+            title.value = list[page].title.toString()
+            subtitle.value = list[page].subtitle.toString()
+            isNew.value = list[page].isNew ?: false
+            isBuy.value = list[page].isBuy ?: false
 
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Top,
-            horizontalAlignment = Alignment.Start) {
-            Box(
-                contentAlignment = Alignment.BottomCenter
-            ) {
-                val painter = rememberAsyncImagePainter(imgUrl.value)
-                Image(
-                    painter = painter,
-                    contentDescription = "",
-                    modifier = Modifier
-                        .padding(8.dp)
-                        .clip(RoundedCornerShape(16.dp))
-                        .fillMaxSize(),
-                    contentScale = ContentScale.Crop
-                )
-                Column(
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.Start,
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(horizontal = 32.dp)
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.Start) {
+                Box(
+                    contentAlignment = Alignment.BottomCenter
                 ) {
-                    if(isNew.value) LabelNew()
-                    Text(text = title.value, color = Color.White)
-                    Text(text = subtitle.value, color = Color.White)
-                    if(isBuy.value) BuyButton()
-
+                    val painter = rememberAsyncImagePainter(imgUrl.value)
+                    Image(
+                        painter = painter,
+                        contentDescription = "",
+                        modifier = Modifier
+                            .padding(8.dp)
+                            .clip(RoundedCornerShape(16.dp))
+                            .fillMaxSize(),
+                        contentScale = ContentScale.Crop
+                    )
+                    Column(
+                        verticalArrangement = Arrangement.SpaceBetween,
+                        horizontalAlignment = Alignment.Start,
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(32.dp)
+                    ) {
+                        if(isNew.value) LabelNew()
+                        MainText(title = title.value, subtitle = subtitle.value)
+                        if(isBuy.value) BuyButton()
+                    }
                 }
             }
         }
     }
+
+
+}
+
+@Composable
+private fun MainText(title: String, subtitle: String) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.Start
+    ) {
+        Box {
+            Text(text = title, color = Color.Black,
+                modifier = Modifier
+                    .blur(20.dp)
+                    .alpha(0.5f)
+                    .offset(x = 0.5.dp, y = 0.5.dp)
+            )
+            Text(text = title, color = Color.White)
+        }
+        Box {
+            Text(text = subtitle, color = Color.Black,
+                modifier = Modifier
+                    .blur(20.dp)
+                    .alpha(0.5f)
+                    .offset(x = 0.5.dp, y = 0.5.dp)
+            )
+            Text(text = subtitle, color = Color.White)
+        }
+    }
+
 }
 
 @Composable
 private fun LabelNew() {
     Box(modifier = Modifier
-        .size(40.dp)
+        .size(32.dp)
         .shadow(3.dp, shape = CircleShape)
         .background(color = colorResource(R.color.main)),
         contentAlignment = Alignment.Center
     ) {
-        Text(text = "New", color = Color.White)
+        Text(
+            text = "New",
+            style = Typography.labelSmall,
+            color = Color.White)
 
     }
 }
@@ -117,7 +158,9 @@ private fun LabelNew() {
 @Composable
 private fun BuyButton() {
     Button(
-        modifier = Modifier.height(50.dp).padding(vertical = 8.dp),
+        modifier = Modifier
+            .height(50.dp)
+            .padding(vertical = 8.dp),
         onClick = { /*TODO*/ },
         shape = RoundedCornerShape(8.dp),
         contentPadding = PaddingValues(0.dp),
