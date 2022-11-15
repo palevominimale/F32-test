39185fa5bc7f31ce1a97fb04d0bd2f7ae2e45a65
palevominimale <badtri.orchestra@gmail.com>
2022-11-15 22:46:35 +0100

    branch with modules

--- app/src/main/java/app/seals/f32test/ui/navigation/NavigationGraph.kt
+++ app/src/main/java/app/seals/f32test/ui/navigation/NavigationGraph.kt
@@ -1,7 +1,6 @@
 package app.seals.f32test.ui.navigation
 
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavDirections
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
